a2abd20e8f1d4c6b1628bc4b981b6a33db9ac62a
Antek Różański <ant.rozanski@gmail.com>
2017-06-09 15:57:58 +0200

    Added Dat operation code and tests.

--- logic/mars/DatOperation.cpp
+++ logic/mars/DatOperation.cpp
@@ -1,14 +1,16 @@
 #include "DatOperation.h"
+#include "ProcessActionRemove.h"
 #include <logic/CoreWarsConstants.h>
+#include <boost/make_shared.hpp>
 
 DatOperation::DatOperation() : MarsOperation(ParserConstants::INSTR_CODE_DAT) {
 
 }
 
 boost::shared_ptr<ProcessAction> DatOperation::runOperation(OperationParamsMixed *operParams) {
-    return nullptr;
+    return boost::shared_ptr<ProcessAction>(new ProcessActionRemove());
 }
 
 boost::shared_ptr<ProcessAction> DatOperation::runOperation(OperationParamsInstructions *operParams) {
-    return nullptr;
+    return boost::shared_ptr<ProcessAction>(new ProcessActionRemove());
 }
--- test_cases/mars/DatOperationTest.cpp
+++ test_cases/mars/DatOperationTest.cpp
@@ -4,32 +4,33 @@
 #include <logic/parser/InstructionCreator.h>
 #include <logic/mars/ProcessActionContinue.h>
 #include <logic/mars/OperationParamsMixed.h>
+#include <logic/mars/ProcessActionRemove.h>
 
-SCENARIO("MovOperationTest: checking mov instr") {
-    boost::shared_ptr<MarsOperation> movOper = OperationFactory::createOperation("MOV");
-    REQUIRE(movOper.get()->getOpCode() == "MOV");
+SCENARIO("DatOperationTest: checking DAT instr") {
+    boost::shared_ptr<MarsOperation> datOper = OperationFactory::createOperation(ParserConstants::INSTR_CODE_DAT);
+    REQUIRE(datOper.get()->getOpCode() == ParserConstants::INSTR_CODE_DAT);
     GIVEN("Params: instruction and number as OperationMixedParams") {
 
         int num = 5;
-        std::string code("DAT");
+        std::string code(ParserConstants::INSTR_CODE_MOV);
         std::string aField("#3");
         std::string bField("$8");
         Instruction instr = InstructionCreator::tryCreate(InstructionData(code, aField, bField));
         OperationParams *mixedParams = new OperationParamsMixed(num, instr);
 
         WHEN("MovOperation is executed") {
-            boost::shared_ptr<ProcessAction> res = mixedParams->accept(movOper);
+            boost::shared_ptr<ProcessAction> res = mixedParams->accept(datOper);
 
             THEN("ProcessAction has correct Type") {
 
-                ProcessActionContinue *con = dynamic_cast<ProcessActionContinue *> (res.get());
+                ProcessActionRemove *con = dynamic_cast<ProcessActionRemove *> (res.get());
                 REQUIRE(con != nullptr);
 
-                AND_THEN("Instruction bValue is correct") {
-                    REQUIRE(instr.getAddressB().get()->getValue() == 8 + 5);
+                AND_THEN("Parameter Instruction bValue has not changed") {
+                    REQUIRE(instr.getAddressB().get()->getValue() == 8);
                 }
-                AND_THEN("Instruction code is correct") {
-                    REQUIRE(instr.getOperation()->getOpCode() == "DAT");
+                AND_THEN("Parameter Instruction code has not changed") {
+                    REQUIRE(instr.getOperation()->getOpCode() == ParserConstants::INSTR_CODE_MOV);
                 }
             }
         }
@@ -37,12 +38,12 @@ SCENARIO("MovOperationTest: checking mov instr") {
     }
     GIVEN("Params: two instructions ") {
 
-        std::string code("DAT");
+        std::string code(ParserConstants::INSTR_CODE_DAT);
         std::string aField("#3");
         std::string bField("$8");
         Instruction instr = InstructionCreator::tryCreate(InstructionData(code, aField, bField));
 
-        std::string codeSec("MOV");
+        std::string codeSec(ParserConstants::INSTR_CODE_MOV);
         std::string aFieldSec("@7");
         std::string bFieldSec("#2");
         Instruction instrSec = InstructionCreator::tryCreate(InstructionData(codeSec, aFieldSec, bFieldSec));
@@ -51,19 +52,19 @@ SCENARIO("MovOperationTest: checking mov instr") {
 
         REQUIRE(doubleInstrParams != nullptr);
         WHEN("MovOperation is executed") {
-            boost::shared_ptr<ProcessAction> res = doubleInstrParams->accept(movOper);
+            boost::shared_ptr<ProcessAction> res = doubleInstrParams->accept(datOper);
             THEN("ProcessAction has correct Type") {
 
-                ProcessActionContinue *con = dynamic_cast<ProcessActionContinue *> (res.get());
+                ProcessActionRemove *con = dynamic_cast<ProcessActionRemove *> (res.get());
                 REQUIRE(con != nullptr);
 
-                AND_THEN("Instruction aValue and bValue are the same as in source instruction (copy)") {
+                AND_THEN("Instruction aValue and bValue did not changed") {
                     instr.addToBValue(5);
-                    REQUIRE(instrSec.getAddressA().get()->getValue() == 3);
-                    REQUIRE(instrSec.getAddressB().get()->getValue() == 8);
+                    REQUIRE(instrSec.getAddressA().get()->getValue() == 7);
+                    REQUIRE(instrSec.getAddressB().get()->getValue() == 2);
                 }
-                AND_THEN("Instruction code is correct") {
-                    REQUIRE(instrSec.getOperation()->getOpCode() == "DAT");
+                AND_THEN("Instruction code did not changed") {
+                    REQUIRE(instrSec.getOperation()->getOpCode() == ParserConstants::INSTR_CODE_MOV);
                 }
             }
         }
